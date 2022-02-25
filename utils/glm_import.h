f8138dfa0198e9b20d4a364e95c1868249ed8999
Satheesh Maheswaran <qms64511@ws448.diamond.ac.uk>
2022-02-25 22:23:07 +0000

    adding changes to utils directory

---
+++ utils/glm_import.h
@@ -0,0 +1,17 @@
+#ifndef __glm__import__
+#define __glm__import__
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_PURE
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtx/string_cast.hpp>
+#include <gtx/vector_angle.hpp>
+
+#define deg2rad(a) ((a)*M_PI/180)
+#define rad2deg(a) ((a) / M_PI * 180)
+
+#endif
+
---
+++ utils/reader/tests/split_by_comma_leaves_dangling_strings.cpp
@@ -0,0 +1,39 @@
+#include "reader/include/FileReader.h"
+
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::string full = "split,me,";
+	//std::vector<std::string> list = split(full, ',');
+
+	std::vector<std::string> list;
+	std::stringstream ss(full);
+	
+	while (ss.good()) {
+		std::string substr;
+		getline(ss, substr, ',');
+		list.push_back(substr);
+	}
+	
+	if ((list.size() != 3) && (list[0] == ""))
+	{
+		return 1;
+	} else return 0;
+	/**
+	if (list.size() != 3)
+	{
+		return 1;
+	}
+	
+	if (list[2] == "")
+	{
+		return 0;
+	}
+	
+	return 1;
+	*/
+}
+
+
